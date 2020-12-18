7255fea9e38c24fe360f17dbced070b836512a87
jorap <jorap@jorap.com>
2020-12-18 14:17:00 +0800

    Removed Dependence From Google Fonts

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -15,14 +15,15 @@
 
         <link rel="home" href="{{ $page->baseUrl }}">
         <link rel="icon" href="/favicon.ico">
+        {{--
         <link href="/blog/feed.atom" type="application/atom+xml" rel="alternate" title="{{ $page->siteName }} Atom Feed">
-
+        --}}
         @if ($page->production)
             <!-- Insert analytics code here -->
         @endif
+        {{--
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-        {{--
         <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,300i,400,400i,700,700i,800,800i" rel="stylesheet">
         --}}
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
